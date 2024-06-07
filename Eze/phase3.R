29022293469efe8f317d8826c37e1b4500f9faec
ezegabriel <chieloka.eze@centre.edu>
2024-06-07 15:04:19 -0400

    holla

--- Eze/phase3.R
+++ Eze/phase3.R
@@ -1,1 +1,21 @@
 # Hello World !!!
+nfl_passing <- read_csv("https://raw.githubusercontent.com/36-SURE/36-SURE.github.io/main/data/nfl_passing.csv")
+
+
+nfl_passing |> 
+  filter(!is.na(pass_length), !is.na(complete_pass)) |> 
+  ggplot(aes(y=complete_pass, x=pass_length))+
+  geom_col()
+
+
+
+nfl_passing |> 
+  group_by(passer_player_name) |> 
+  count(touchdown)
+
+
+nfl_passing |> 
+  ggplot(aes(y=wpa, x=epa))+
+  geom_point(alpha=.5, color='darkblue')+
+  geom_smooth()+
+  coord_flip() 
